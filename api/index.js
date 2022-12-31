4069495fe7d1e9ca3f3f23062a408d65a05e516d
Ali <AliNazarii7394@gmail.com>
2022-12-31 14:29:14 +0330

    mongoose config done

--- api/index.js
+++ api/index.js
@@ -1,1 +1,23 @@
-console.log("hello");
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB conection successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/api/test", (req,res) => {
+  res.send("test done")
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("backen server is running");
+});

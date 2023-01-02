3dc0a876a8537d68057cace0687b133125c6a394
Ali <AliNazarii7394@gmail.com>
2023-01-02 16:53:14 +0330

    Product Router Created

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,4 +1,73 @@
 import express from "express";
+import Product from "../models/Product.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been removed");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET ONE
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET ALL
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+    if (qNew) {
+      products = await Product.find()
+        .sort({ createdAt: -1 })
+        .limit(+qNew || 1);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
